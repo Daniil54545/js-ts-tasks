d1e877a12d6291671abf6676037060015284c5b7
Daniil54545 <seliavkodanik@gmail.com>
2024-11-06 10:12:02 +0200

    Update volumeCone.js

--- src/volumeCone.js
+++ src/volumeCone.js
@@ -1,9 +1,16 @@
 /**
- * Given cone height and radius (numbers). Evaluate volume of a cone
- * @param {number} h - height
- * @param {number} r - radius
- * @returns {number}
+ * Calculate the volume of a cone given its height and radius
+ * @param {number} r - The radius of the cone
+ * @param {number} h - The height of the cone
+ * @returns {number} - The volume of the cone
  */
-module.exports.volumeCone = function volumeCone(h, r) {
-  throw new Error('Not implemented');
+module.exports.calculateConeVolume = function calculateConeVolume(r, h) {
+  if (typeof r !== 'number' || typeof h !== 'number' || r < 0 || h < 0) {
+    throw new Error('Invalid input');
+  }
+
+  const volume = (1/3) * Math.PI * Math.pow(r, 2) * h;
+  return volume;
 };
+
+console.log(module.exports.calculateConeVolume(3, 5)); 
