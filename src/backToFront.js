6c4f1a86529c2c127090c3698c0d64758737de5c
Daniil54545 <seliavkodanik@gmail.com>
2024-11-06 10:09:31 +0200

    Update backToFront.js

--- src/backToFront.js
+++ src/backToFront.js
@@ -1,9 +1,20 @@
 /**
  * Copy and paste defined amount of a string's symbols to the front and back of a string
- * @param {string} str
- * @param {number} symbolsCount
- * @returns {string}
+ * @param {string} str - The input string
+ * @param {number} symbolsCount - The number of symbols to copy to the front and back
+ * @returns {string} - The modified string
  */
 module.exports.backToFront = function backToFront(str, symbolsCount) {
-  throw new Error('Not implemented');
-};
+  function copyAndPasteSymbols(str, num) {
+    if (num > str.length) {
+      return 'The number of symbols to copy exceeds the length of the string.';
+    }
+    let part = str.slice(0, num);
+    return part + str + part;
+  }
+  
+  let originalString = 'hello';
+  let numSymbols = 2;
+  alert(copyAndPasteSymbols(originalString, numSymbols));
+
+}
