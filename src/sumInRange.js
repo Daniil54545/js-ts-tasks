aca0675735b9e91dc14db06f6fc7ee3d4468e9b5
Daniil54545 <seliavkodanik@gmail.com>
2024-11-06 10:11:24 +0200

    Update sumInRange.js

--- src/sumInRange.js
+++ src/sumInRange.js
@@ -1,9 +1,21 @@
 /**
  * Find a sum in a range of consecutive numbers
- * @param {number|string} start
- * @param {number|string} end
- * @returns {number}
+ * @param {number|string} start - starting number of the range
+ * @param {number|string} end - ending number of the range
+ * @returns {number} - sum of numbers in the range
  */
 module.exports.sumInRange = function sumInRange(start, end) {
-  throw new Error('Not implemented');
+  start = Number(start);
+  end = Number(end);
+ 
+  if (isNaN(start) || isNaN(end) || start > end) {
+    throw new Error('Invalid input');
+  }
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+return sum;
 };
+
+console.log(module.exports.sumInRange(1, 5));
