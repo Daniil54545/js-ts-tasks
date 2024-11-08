e6066431bb6f0508f1d3b090edd70a33186c90b8
Daniil54545 <seliavkodanik@gmail.com>
2024-11-08 14:18:54 +0200

    Update volumeCone.js

--- src/volumeCone.js
+++ src/volumeCone.js
@@ -5,12 +5,10 @@
  * @returns {number} - The volume of the cone
  */
 module.exports.calculateConeVolume = function calculateConeVolume(r, h) {
-  if (typeof r !== 'number' || typeof h !== 'number' || r < 0 || h < 0) {
-    throw new Error('Invalid input');
-  }
+    if (typeof r !== 'number' || typeof h !== 'number' || r < 0 || h < 0) {
+      throw new Error('Invalid input');
+    }
 
-  const volume = (1/3) * Math.PI * Math.pow(r, 2) * h;
-  return volume;
-};
-
-console.log(module.exports.calculateConeVolume(3, 5)); 
+    const volume = (1/3) * Math.PI * Math.pow(r, 2) * h;
+    return volume;
+  };
