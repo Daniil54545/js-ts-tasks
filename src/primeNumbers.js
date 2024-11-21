b6c66d9310196114d8630db373801a51cb21f61e
Daniil54545 <seliavkodanik@gmail.com>
2024-11-21 12:48:02 +0200

    Update primeNumbers.js

--- src/primeNumbers.js
+++ src/primeNumbers.js
@@ -4,5 +4,26 @@
  * @returns {function}
  */
 module.exports.primeNumbers = function primeNumbers(highestNumber) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+  }
+
+  return function (low, high) {
+    if (low < 0) low = 0;
+    if (high > highestNumber) high = highestNumber;
+    
+    const primes = [];
+    for (let i = low; i <= high; i++) {
+      if (isPrime(i)) {
+        primes.push(i);
+      }
+    }
+    return primes;
+  };
 };
